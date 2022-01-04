d46e6ff2f8a81dac59efde4d4884f173ab3bb81e
Ludeeus <ludeeus@ludeeus.dev>
2022-01-04 14:23:08 +0000

    rename to client_metadata

--- pycognito/__init__.py
+++ pycognito/__init__.py
@@ -336,7 +336,7 @@ class Cognito:
 
         self.custom_attributes = custom_attributes
 
-    def register(self, username, password, attr_map=None, metadata=None):
+    def register(self, username, password, attr_map=None, client_metadata=None):
         """
         Register the user. Other base attributes from AWS Cognito User Pools
         are  address, birthdate, email, family_name (last name), gender,
@@ -346,7 +346,7 @@ class Cognito:
         :param username: User Pool username
         :param password: User Pool password
         :param attr_map: Attribute map to Cognito's attributes
-        :param metadata: Metadata about the user that will be used for ClientMetadata
+        :param client_metadata: Metadata about the user that will be used for ClientMetadata
         :return response: Response from Cognito
 
         Example response::
@@ -373,8 +373,8 @@ class Cognito:
             "Password": password,
             "UserAttributes": cognito_attributes,
         }
-        if metadata is not None:
-            params["ClientMetadata"] = metadata
+        if client_metadata is not None:
+            params["ClientMetadata"] = client_metadata
         self._add_secret_hash(params, "SecretHash")
         response = self.client.sign_up(**params)
 
